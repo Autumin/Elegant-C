0f1528df54de80d240a9ebaf7199093d24dfae18
Autumin <true-meaning@outlook.com>
2017-02-16 17:36:58 +0800

    init commit
    A char is just a small integer.

---
+++ atoi.c
@@ -0,0 +1,18 @@
+/* atoi:  convert s to integer */
+int atoi(char s[])
+{
+    int i, n;
+    n = 0;
+    for (i = 0; s[i] >= '0' && s[i] <= '9'; ++i)
+        n = 10 * n + (s[i] - '0');
+    return n;
+}
+
+/* lower:  convert c to lower case; ASCII only */
+int lower(int c)
+{
+    if (c >= 'A' && c <= 'Z')
+        return c + 'a' - 'A';
+    else
+        return c;
+}
